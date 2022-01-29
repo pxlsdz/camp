0a7495e7142d308ab8f66b10185896aced4c2e50
guobh <guobh@magus.ink>
2022-01-29 16:30:35 +0800

    feat:login and logout and whoami.

--- routers/api/v1/auth.go
+++ routers/api/v1/auth.go
@@ -1,1 +1,89 @@
 package v1
+
+import (
+	"camp/infrastructure/stores/mysql"
+	"camp/models"
+	"camp/types"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+func AuthMiddleWare() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		// 获取客户端cookie并校验
+		if cookie, err := c.Cookie("camp-session"); err == nil && cookie != "" {
+			c.Next()
+			return
+		}
+		// 返回错误,未授权
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "unAuthorized"})
+		// 若验证不通过，不再调用后续的函数处理
+		c.Abort()
+		return
+	}
+}
+
+func Login(c *gin.Context) {
+
+	fmt.Println("login")
+	var json types.LoginRequest
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, "json解析失败")
+		return
+	}
+
+	username := json.Username
+	password := json.Password
+
+	db := mysql.GetDb()
+	var member models.Member
+	err := db.Where("username = ?", username).First(&member)
+	//err := db.Select(&member, "select username from member where username='guoguo'")
+	//result := db.Select("select username from member where username=?", username)
+	if err.Error != nil || member.Password != password {
+		c.JSON(http.StatusOK, types.LoginResponse{Code: types.WrongPassword})
+		return
+	}
+
+	// 登录成功，设置cookie
+	c.SetCookie("camp-session", strconv.FormatInt(member.ID, 10), 120, "/",
+		"127.0.0.1", false, true)
+
+	c.JSON(http.StatusOK, types.LoginResponse{
+		Code: types.OK,
+		Data: struct {
+			UserID string
+		}{strconv.FormatInt(member.ID, 10)},
+	})
+}
+
+func Logout(c *gin.Context) {
+	//建立同名cookie进行覆盖
+	c.SetCookie("camp-session", "value_cookie", -1, "/",
+		"127.0.0.1", false, true)
+	// 返回信息
+	c.JSON(http.StatusOK, types.LogoutResponse{
+		Code: types.OK,
+	})
+}
+
+func Whoami(c *gin.Context) {
+
+	cookie_uid, _ := c.Cookie("camp-session")
+	db := mysql.GetDb()
+	var member models.Member
+
+	db.Where("id = ?", cookie_uid).First(&member)
+	c.JSON(http.StatusOK, types.WhoAmIResponse{
+		Code: types.OK,
+		Data: types.TMember{
+			UserID:   strconv.FormatInt(member.ID, 10),
+			Nickname: member.Nickname,
+			Username: member.Username,
+			UserType: member.UserType,
+		},
+	})
+}
--- routers/router.go
+++ routers/router.go
@@ -7,30 +7,32 @@ import "camp/routers/api/v1"
 
 func RegisterRouter(r *gin.Engine) {
 	g := r.Group("/api/v1")
-	// 成员管理
-	g.POST("/member/create", v1.CreateMember)
-	g.GET("/member", v1.GetMember)
-	g.GET("/member/list", v1.GetMemberList)
-	g.POST("/member/update", v1.UpdateMember)
-	g.POST("/member/delete", v1.DeleteMember)
+	{
+		// 成员管理
+		g.POST("/member/create", v1.CreateMember)
+		g.GET("/member", v1.GetMember)
+		g.GET("/member/list", v1.GetMemberList)
+		g.POST("/member/update", v1.UpdateMember)
+		g.POST("/member/delete", v1.DeleteMember)
 
-	// 登录
+		// 登录
 
-	g.POST("/auth/login")
-	g.POST("/auth/logout")
-	g.GET("/auth/whoami")
+		g.POST("/auth/login", v1.Login)
+		g.POST("/auth/logout", v1.Logout)
+		g.GET("/auth/whoami", v1.AuthMiddleWare(), v1.Whoami)
 
-	// 排课
-	g.POST("/course/create")
-	g.GET("/course/get")
+		// 排课
+		g.POST("/course/create")
+		g.GET("/course/get")
 
-	g.POST("/teacher/bind_course")
-	g.POST("/teacher/unbind_course")
-	g.GET("/teacher/get_course")
-	g.POST("/course/schedule")
+		g.POST("/teacher/bind_course")
+		g.POST("/teacher/unbind_course")
+		g.GET("/teacher/get_course")
+		g.POST("/course/schedule")
 
-	// 抢课
-	g.POST("/student/book_course")
-	g.GET("/student/course")
+		// 抢课
+		g.POST("/student/book_course")
+		g.GET("/student/course")
+	}
 
 }
