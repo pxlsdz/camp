bf9eac21dc579f7f25f60e05a0b22ecc99adec52
YJ <421791975@qq.com>
2022-02-13 17:09:25 +0800

    add:GetTCourseByID function

--- repository/repository.go
+++ repository/repository.go
@@ -1,10 +1,13 @@
 package repository
 
 import (
+	redis "camp/infrastructure/stores/myRedis"
 	"camp/infrastructure/stores/mysql"
 	"camp/models"
 	"camp/types"
+	"context"
 	"errors"
+	"fmt"
 	"gorm.io/gorm"
 )
 
@@ -58,5 +61,55 @@ func GetCapCourseById(id int64, cap *int) types.ErrNo {
 		}
 	}
 	*cap = course.Cap
+	return types.OK
+}
+
+func GetTCourseByID(id int64, tCourse *types.TCourse) types.ErrNo {
+
+	ctx := context.Background()
+	client := redis.GetClient()
+	db := mysql.GetDb()
+
+	key := fmt.Sprintf(types.TCourseKey, id)
+
+	n, err1 := client.Exists(ctx, key).Result()
+	if err1 != nil {
+		return types.UnknownError
+	} else if n > 0 { //key存在于redis中
+		val, err := client.HGetAll(ctx, key).Result()
+		// 判断查询是否出错
+		if err != nil {
+			return types.UnknownError
+		} else {
+			tCourse.CourseID = fmt.Sprintf("%d", id)
+			tCourse.TeacherID = val["TeacherID"]
+			tCourse.Name = val["Name"]
+		}
+	} else { //key不存在于redis中
+
+		var course models.Course
+		//先在数据库中查询
+		if err := db.Take(&course, id).Error; err != nil {
+			// 判断课程是否存在
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				return types.CourseNotExisted
+			} else {
+				return types.UnknownError
+			}
+		}
+
+		//存在，写入redis中,设置十分钟过期
+		client.HSet(ctx, key, map[string]interface{}{
+			"Name":      course.Name,
+			"TeacherID": course.TeacherID,
+		})
+		client.Expire(ctx, key, 600)
+
+		tCourse.CourseID = fmt.Sprintf("%d", id)
+		tCourse.TeacherID = fmt.Sprintf("%d", course.TeacherID)
+		tCourse.Name = course.Name
+
+	}
+
 	return types.OK
 }
