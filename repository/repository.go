cb746739f2bb20c5e21a44d6c7414c4ab4cad9f3
sdz <sdz@zju.edu.com>
2022-02-17 09:02:43 +0800

    fix repository

--- repository/repository.go
+++ repository/repository.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	redis3 "github.com/go-redis/redis/v8"
 	"gorm.io/gorm"
 	"strconv"
 	"time"
@@ -116,33 +117,60 @@ func GetTCourseByIDs(ids []int64) (tCourses []types.TCourse, code types.ErrNo) {
 	db := mysql.GetDb()
 	//转化为redis中对应的keys
 	length := len(ids)
-	tCourses = make([]types.TCourse, length)
+	//初始化
+	tCourses = make([]types.TCourse, 0, length)
 	keys := make([]string, length)
+	//转化id位redis对应的key
 	for i, id := range ids {
 		keys[i] = fmt.Sprintf(types.TCourseKey, id)
 	}
 
+	//初始化redis中不存在的id列表
 	idsNotExistRedis := make([]int64, 0)
 
-	for i, key := range keys {
+	//管道
+	pipeline := client.Pipeline()
+
+	for _, key := range keys {
 		//从redis中获取hmap，会返回一个map集合
-		val, err := client.HGetAll(ctx, key).Result()
-		// 判断查询是否出错
-		if err != nil {
-			code = types.UnknownError
-			return
-		} else if len(val) != 0 { //如果集合里面有元素，则该键存在于redis中
-			tCourse := types.TCourse{
-				fmt.Sprintf("%d", ids[i]),
-				val["Name"],
-				val["TeacherID"],
-			}
-			tCourses = append(tCourses, tCourse)
-		} else { //key不存在于redis中
-			idsNotExistRedis = append(idsNotExistRedis, ids[i])
-		}
+		pipeline.HGetAll(ctx, key)
 	}
 
+	//执行
+	exec, err := pipeline.Exec(ctx)
+
+	if err != nil {
+		if err != redis.Nil {
+			code = types.UnknownError
+			return
+		}
+		// 注意这里如果某一次获取时出错（常见的redis.Nil），返回的err即不为空
+		// 如果需要处理redis.Nil为默认值，此处不能直接return
+	}
+
+	for index, cmdRes := range exec {
+		// 此处断言类型为在for循环内执行的命令返回的类型,上面HGet返回的即为*redis2.StringStringMapCmd
+		// 处理方式和直接调用同样处理即可
+		cmd, ok := cmdRes.(*redis3.StringStringMapCmd)
+		if ok {
+			val, err := cmd.Result()
+			if err != nil {
+				code = types.UnknownError
+				return
+			}
+			//判断是否返回的为空，为空map则不存在
+			if len(val) != 0 {
+				tCourse := types.TCourse{
+					fmt.Sprintf("%d", ids[index]),
+					val["Name"],
+					val["TeacherID"],
+				}
+				tCourses = append(tCourses, tCourse)
+			} else { //key不存在于redis中
+				idsNotExistRedis = append(idsNotExistRedis, ids[index])
+			}
+		}
+	}
 	if len(idsNotExistRedis) != 0 {
 		//处理未在redis找到的课程id
 		var courses []models.Course
